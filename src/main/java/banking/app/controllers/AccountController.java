4200463c0e7875072c60c7fa5ce6f203f339f4a7
Ali <ali.saeed@gitlab.hiast.edu.sy>
2025-03-26 02:03:36 +0300

    adding account controller

---
+++ src/main/java/banking/app/controllers/AccountController.java
@@ -0,0 +1,31 @@
+package banking.app.controllers;
+
+import banking.app.dtos.AccountDto;
+import banking.app.dtos.CreateAccountDto;
+import banking.app.services.IAccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/accounts")
+public class AccountController {
+
+    @Autowired
+    private IAccountService accountService;
+
+    public AccountController(IAccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    //Add Account Rest API
+    @PostMapping("/add")
+    public ResponseEntity<AccountDto> addAccount(@RequestBody CreateAccountDto accountDto){
+        return new ResponseEntity<>(accountService.createAccount(accountDto), HttpStatus.CREATED);
+    }
+}
